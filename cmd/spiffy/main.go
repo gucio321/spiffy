eba1db32665d82d611aa91d4cd947f295f811d08
gucio321 <gucio321@protonmail.com>
2024-10-02 12:15:14 +0200

    main: improve programm

--- cmd/spiffy/main.go
+++ cmd/spiffy/main.go
@@ -10,31 +10,45 @@ import (
 	pkg "github.com/gucio321/spiffy/pkg"
 )
 
+type flags struct {
+	inputFilePath  string
+	outputFilePath string
+	scale          float64
+}
+
 func main() {
-	var inputFilePath string
-	flag.StringVar(&inputFilePath, "i", "", "input file path")
+	var f flags
+	flag.StringVar(&f.inputFilePath, "i", "", "input file path")
+	flag.StringVar(&f.outputFilePath, "o", "", "output file path")
+	flag.Float64Var(&f.scale, "s", 1.0, "scale factor")
 	flag.Parse()
 
-	if _, err := os.Stat(inputFilePath); os.IsNotExist(err) {
+	if _, err := os.Stat(f.inputFilePath); os.IsNotExist(err) {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	data, err := os.ReadFile(inputFilePath)
+	data, err := os.ReadFile(f.inputFilePath)
 	if err != nil {
-		glg.Fatalf("Cannot read file %s: %v", inputFilePath, err)
+		glg.Fatalf("Cannot read file %s: %v", f.inputFilePath, err)
 	}
 
 	result, err := pkg.Parse(data)
 	if err != nil {
-		glg.Fatalf("Cannot parse file %s: %v", inputFilePath, err)
+		glg.Fatalf("Cannot parse file %s: %v", f.inputFilePath, err)
 	}
 
-	result.Scale(0.1)
+	result.Scale(float32(f.scale))
 	gcode, err := result.GCode()
 	if err != nil {
 		glg.Fatalf("Cannot generate GCode: %v", err)
 	}
 
 	fmt.Println(gcode)
+
+	if f.outputFilePath != "" {
+		if err := os.WriteFile(f.outputFilePath, []byte(gcode), 0644); err != nil {
+			glg.Fatalf("Cannot write file %s: %v", f.outputFilePath, err)
+		}
+	}
 }
