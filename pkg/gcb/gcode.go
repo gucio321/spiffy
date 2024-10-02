635a4dd17930c4d0d1b1870608fcad2e573e63fc
gucio321 <gucio321@protonmail.com>
2024-10-02 12:00:53 +0200

    gcb refactor: add ContinousLine feature

--- pkg/gcb/gcode.go
+++ pkg/gcb/gcode.go
@@ -71,16 +71,18 @@ type GCodeBuilder struct {
 	isDrawing           bool
 	currentP            BetterPoint[HardwareAbsolutePos]
 	preamble, postamble string
+	continousLine       bool
 }
 
 // NewGCodeBuilder creates new GCodeBuilder with default values.
 func NewGCodeBuilder() *GCodeBuilder {
 	return &GCodeBuilder{
-		currentP:  BetterPoint[HardwareAbsolutePos]{BaseX, BaseY},
-		depth:     BaseDepth,
-		headSize:  DefaultHeadSize,
-		preamble:  DefaultPreamble,
-		postamble: DefaultPostamble,
+		currentP:      BetterPoint[HardwareAbsolutePos]{BaseX, BaseY},
+		depth:         BaseDepth,
+		headSize:      DefaultHeadSize,
+		preamble:      DefaultPreamble,
+		postamble:     DefaultPostamble,
+		continousLine: false,
 	}
 }
 
@@ -119,6 +121,30 @@ func (b *GCodeBuilder) Down() *GCodeBuilder {
 
 	b.isDrawing = true
 
+	return b
+}
+
+// StartContinousLine starts drawing a continous line.
+// Every draw command's starting point should be b.Current() (and this will be checked and will panic if not true).
+// Then, no Up()/Down() will be called automatically.
+func (b *GCodeBuilder) StartContinousLine() *GCodeBuilder {
+	if b.continousLine {
+		glg.Fatalf("StartContinousLine called, but already drawing continous line! %s", b.code)
+	}
+
+	b.Down()
+	b.continousLine = true
+	return b
+}
+
+// StopContinousLine stops drawing a continous line.
+func (b *GCodeBuilder) StopContinousLine() *GCodeBuilder {
+	if !b.continousLine {
+		glg.Fatalf("StopContinousLine called, but not drawing continous line! %s", b.code)
+	}
+
+	b.Up()
+	b.continousLine = false
 	return b
 }
 
@@ -165,14 +191,22 @@ func (b *GCodeBuilder) Separator() *GCodeBuilder {
 func (b *GCodeBuilder) DrawLine(p0, p1 BetterPoint[AbsolutePos]) *GCodeBuilder {
 	b.Commentf("BEGIN DrawLine(%v, %v)", p0, p1)
 
-	// 1.1: go to x0, y0
-	b.Move(p0)
-	// 1.2: start drawing
-	b.Down()
+	if !b.continousLine {
+		// 1.1: go to x0, y0
+		b.Move(p0)
+		// 1.2: start drawing
+		b.Down()
+	} else {
+		if p0 != b.Current() {
+			glg.Fatalf("DrawLine called, but current position is not the same as starting point! %v != %v", p0, b.Current())
+		}
+	}
 	// 1.3: go to x1, y1
 	b.Move(p1)
 	// 1.4: stop drawing
-	b.Up()
+	if !b.continousLine {
+		b.Up()
+	}
 
 	b.Commentf("END DrawLine(%f, %f)", p0, p1)
 
@@ -180,25 +214,29 @@ func (b *GCodeBuilder) DrawLine(p0, p1 BetterPoint[AbsolutePos]) *GCodeBuilder {
 }
 
 // DrawPath draws a path of lines. Closed if true, will automatically close the path by drawing line from path[n] to path[0].
-func (b *GCodeBuilder) DrawPath(closed bool, path ...BetterPoint[AbsolutePos]) *GCodeBuilder {
-	b.Commentf("BEGIN DrawPath(%v, %v)", closed, path)
+func (b *GCodeBuilder) DrawLines(path ...BetterPoint[AbsolutePos]) *GCodeBuilder {
+	b.Commentf("BEGIN DrawPath(%v)", path)
 
-	b.Move(path[0])
-	b.Down()
+	if !b.continousLine {
+		b.Move(path[0])
+		b.Down()
+	} else {
+		if path[0] != b.Current() {
+			glg.Fatalf("DrawPath called, but current position is not the same as starting point! %v != %v", path[0], b.Current())
+		}
+	}
+
 	for i := 1; i < len(path); i++ {
 		b.Commentf("Line %d", i)
 		p0 := path[i]
 		b.Move(p0)
 	}
 
-	if closed {
-		b.Comment("Close path")
-		b.Move(path[0])
+	if b.continousLine {
+		b.Up()
 	}
 
-	b.Up()
-
-	b.Commentf("END DrawPath(%v, %v)", closed, path)
+	b.Commentf("END DrawPath(%v)", path)
 
 	return b
 }
@@ -214,12 +252,22 @@ func (b *GCodeBuilder) DrawCircle(pImg BetterPoint[AbsolutePos], r float32) *GCo
 		Y: pImg.Y + AbsolutePos(r),
 	}
 
-	b.Move(baseP)
+	if !b.continousLine {
+		b.Move(baseP)
+		b.Down()
+	} else {
+		if baseP != b.Current() {
+			glg.Fatalf("DrawCircle called, but current position is not the same as starting point! %v != %v", baseP, b.Current())
+		}
+	}
+
 	// 1.1: do circle
 	relP := b.absToRel(p)
-	b.Down()
 	b.code += fmt.Sprintf("G2 I%[1]f J%[2]f ; Draw circle with center in %[1]f and %[2]f with radius %[3]f\n", relP.X, relP.Y, r)
-	b.Up()
+
+	if !b.continousLine {
+		b.Up()
+	}
 
 	b.Commentf("END DrawCircle(%f, %f)", pImg, r)
 	return b
@@ -253,7 +301,15 @@ func (b *GCodeBuilder) DrawSector(pImg BetterPoint[AbsolutePos], radius float32,
 		AbsolutePos(math.Sin(float64(start)) * float64(radius)),
 	})
 
-	b.Move(baseP)
+	if !b.continousLine {
+		b.Move(baseP)
+		b.Down()
+	} else {
+		if baseP != b.Current() {
+			glg.Fatalf("DrawSector called, but current position is not the same as starting point! %v != %v", baseP, b.Current())
+		}
+	}
+
 	// 1.1: find final x,y
 	finalP := pImg.Add(BetterPoint[AbsolutePos]{
 		AbsolutePos(math.Cos(float64(end)) * float64(radius)),
@@ -263,10 +319,12 @@ func (b *GCodeBuilder) DrawSector(pImg BetterPoint[AbsolutePos], radius float32,
 	relFinalP := b.absToRel(translate(finalP))
 	// 1.2: do circle
 	relP := b.absToRel(p)
-	b.Down()
 	b.code += fmt.Sprintf("G3 I%[1]f J%[2]f X%[3]f Y%[4]f; Draw circle with center in %[1]f and %[2]f with radius %[5]f\n", relP.X, relP.Y, relFinalP.X, relFinalP.Y, radius)
 	b.currentP = translate(finalP)
-	b.Up()
+
+	if !b.continousLine {
+		b.Up()
+	}
 
 	b.Commentf("END DrawSector(%v, %f, %f, %f)", p, radius, start, end)
 	return b
@@ -276,13 +334,23 @@ func (b *GCodeBuilder) DrawSector(pImg BetterPoint[AbsolutePos], radius float32,
 func (b *GCodeBuilder) DrawRect(p0, p1 BetterPoint[AbsolutePos]) *GCodeBuilder {
 	b.Commentf("BEGIN DrawRect(%v, %v)", p0, p1)
 
-	b.Move(p0)
-	b.Down()
+	if !b.continousLine {
+		b.Move(p0)
+		b.Down()
+	} else {
+		if p0 != b.Current() {
+			glg.Fatalf("DrawRect called, but current position is not the same as starting point! %v != %v", p0, b.Current())
+		}
+	}
+
 	b.Move(p1)
 	b.Move(p1)
 	b.Move(p0)
 	b.Move(p0)
-	b.Up()
+
+	if !b.continousLine {
+		b.Up()
+	}
 
 	b.Commentf("END DrawRect(%v, %v)", p0, p1)
 
@@ -314,24 +382,31 @@ func (b *GCodeBuilder) DrawRectFilled(p0, p1 BetterPoint[AbsolutePos]) *GCodeBui
 func (b *GCodeBuilder) DrawBezierCubic(start, end, control1, control2 BetterPoint[AbsolutePos]) *GCodeBuilder {
 	b.Commentf("BEGIN DrawBezierCubic(%v, %v, %v, %v)", start, end, control1, control2)
 
-	// 1.0: move to start
-	b.Move(start)
-	// 1.1: calculate control point 1 (as relative to start)
-	fmt.Println(b.currentP)
-	fmt.Println(control1)
+	if !b.continousLine {
+		// 1.0: move to start
+		b.Move(start)
+		// 1.1: start drawing
+		b.Down()
+	} else {
+		if start != b.Current() {
+			glg.Fatalf("DrawBezierCubic called, but current position is not the same as starting point! %v != %v", start, b.Current())
+		}
+	}
+
+	// 1.2: calculate control point 1 (as relative to start)
 	control1Rel := b.absToRel(translate(control1))
 	// 1.3: find relative end pos
 	endRel := b.absToRel(translate(end))
-	// 1.3: calculate control point 2 (as relative to end)
+	// 1.4: calculate control point 2 (as relative to end)
 	// according to doc it should be control2-end
 	control2Rel := control2.Add(end.Mul(-1))
-	// 1.4: start drawing
-	b.Down()
 	// 1.5: draw
 	endHwAbs := translate(end)
 	b.code += fmt.Sprintf("G5 I%f J%f P%f Q%f X%f Y%f ; Finish at X %f Y %f\n", control1Rel.X, control1Rel.Y, control2Rel.X, control2Rel.Y, endRel.X, endRel.Y, endHwAbs.X, endHwAbs.Y)
-	// 1.6: stop drawing
-	b.Up()
+	if !b.continousLine {
+		// 1.6: stop drawing
+		b.Up()
+	}
 	// 1.7: update current position
 	b.currentP = endHwAbs
 
@@ -339,6 +414,7 @@ func (b *GCodeBuilder) DrawBezierCubic(start, end, control1, control2 BetterPoin
 	return b
 }
 
+// Current returns current position.
 func (b *GCodeBuilder) Current() BetterPoint[AbsolutePos] {
 	return Redefine[AbsolutePos](b.currentP.Add(BetterPt(HardwareAbsolutePos(-MinX), HardwareAbsolutePos(-MinY))))
 }
