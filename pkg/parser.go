f60981634f0c510dcc746e22520a3b56ec5777e9
gucio321 <gucio321@protonmail.com>
2024-10-06 19:18:17 +0200

    spiffy: use external svg parser
    also, rotates everything (-Y)

--- pkg/parser.go
+++ pkg/parser.go
@@ -1,15 +1,14 @@
 package spiffy
 
-import (
-	"encoding/xml"
-)
+import "github.com/rustyoz/svg"
 
 func Parse(data []byte) (result *Spiffy, err error) {
 	// 0.0: initialize
 	result = NewSpiffy()
 
 	// 1.0: unmarshal xml
-	if err := xml.Unmarshal(data, result); err != nil {
+	// TODO: 2nd arg is "name" (what the hell it means?) and 3rd i "scale" (probably could be 1
+	if result.svg, err = svg.ParseSvg(string(data), "", 1); err != nil {
 		return nil, err
 	}
 
--- pkg/spiffy.go
+++ pkg/spiffy.go
@@ -8,86 +8,13 @@ import (
 
 	"github.com/gucio321/spiffy/pkg/gcb"
 	"github.com/kpango/glg"
+	"github.com/rustyoz/svg"
 )
 
 type Spiffy struct {
-	scale     float32
+	scale     float64
 	noComment bool
-	Defs      Defs  `xml:"defs"`
-	Graph     Graph `xml:"g"`
-}
-
-type Defs struct {
-	Rects   []Rect           `xml:"rect"`
-	Linears []LinearGradient `xml:"linearGradient"`
-}
-
-type Rect struct {
-	X  float32 `xml:"x,attr"`
-	Y  float32 `xml:"y,attr"`
-	W  float32 `xml:"width,attr"`
-	H  float32 `xml:"height,attr"`
-	Id string  `xml:"id,attr"`
-}
-
-type LinearGradient struct {
-	Id    string  `xml:"id,attr"`
-	X1    float32 `xml:"x1,attr"`
-	Y1    float32 `xml:"y1,attr"`
-	X2    float32 `xml:"x2,attr"`
-	Y2    float32 `xml:"y2,attr"`
-	Stops []Stop  `xml:"stop"` // this programm wil not use that but I'll keep it just in case
-}
-
-type Stop struct {
-	Offset float32 `xml:"offset,attr"`
-	Style  string  `xml:"style,attr"`
-}
-
-type Graph struct {
-	Circles []Circle    `xml:"circle"`
-	Paths   []Path      `xml:"path"`
-	Rects   []GraphRect `xml:"rect"`
-	Texts   []Text      `xml:"text"`
-}
-
-type Circle struct {
-	Cx   float32 `xml:"cx,attr"`
-	Cy   float32 `xml:"cy,attr"`
-	R    float32 `xml:"r,attr"`
-	Fill string  `xml:"fill,attr"`
-}
-
-type Path struct {
-	Style string `xml:"style,attr"`
-	D     string `xml:"d,attr"`
-	ID    string `xml:"id,attr"`
-}
-
-type GraphRect struct {
-	Style string  `xml:"style,attr"`
-	ID    string  `xml:"id,attr"`
-	X     float32 `xml:"x,attr"`
-	Y     float32 `xml:"y,attr"`
-	W     float32 `xml:"width,attr"`
-	H     float32 `xml:"height,attr"`
-}
-
-type Text struct {
-	ID    string  `xml:"id,attr"`
-	Tspan []Tspan `xml:"tspan"`
-}
-
-type Tspan struct {
-	X      float32 `xml:"x,attr"`
-	Y      float32 `xml:"y,attr"`
-	ID     string  `xml:"id,attr"`
-	Tspan2 Tspan2  `xml:"tspan"`
-}
-
-type Tspan2 struct {
-	Style string `xml:"style,attr"`
-	Text  string `xml:",chardata"`
+	svg       *svg.Svg
 }
 
 func NewSpiffy() *Spiffy {
@@ -96,8 +23,9 @@ func NewSpiffy() *Spiffy {
 	}
 }
 
+// TODO: fix types
 func (s *Spiffy) Scale(scale float32) *Spiffy {
-	s.scale = scale
+	s.scale = float64(scale)
 	return s
 }
 
@@ -114,99 +42,152 @@ func (s *Spiffy) GCode() (*gcb.GCodeBuilder, error) {
 
 	// 1.0: draw paths
 	builder.Comment("Drawing PATHS from SVG")
-	for lineIdx, line := range s.Graph.Paths {
-		builder.Commentf("Drawing path %d", lineIdx)
-		txts := strings.Split(line.D, " ")
-		var currentType PathType
-		start := gcb.BetterPoint[gcb.AbsolutePos]{0, 0}
+	parsedData, parsedErr := s.svg.ParseDrawingInstructions()
+	if err != nil {
+		return builder, err
+	}
 
-		builder.BeginContinousLine()
-		for i := 0; i < len(txts); {
-			t := txts[i]
-			glg.Debugf("Proessing %d/%d: %s", i, len(txts), t)
-			pathType, ok := PathTypeEnum[t]
-			if ok {
-				currentType = pathType
-				glg.Debugf("Setting current operation type to %s", currentType)
-				i++
+	builder.BeginContinousLine()
+reading:
+	for {
+		select {
+		case cmd := <-parsedData:
+			if cmd == nil {
+				builder.EndContinousLine()
+				break reading
 			}
 
-			var points []gcb.BetterPoint[gcb.AbsolutePos]
-			switch currentType {
-			case PathMoveToRel, PathMoveToAbs:
+			switch cmd.Kind {
+			case svg.MoveInstruction:
 				builder.EndContinousLine()
-				points, err = s.readNPts(txts, &i, 1)
-				if err != nil {
-					return builder, err
-				}
-
-				if currentType == PathMoveToRel {
-					for i, pt := range points {
-						points[i] = builder.RelToAbs(gcb.Redefine[gcb.RelativePos](pt))
-					}
-				}
-
-				glg.Debugf("Moving to %v", points[0])
-				start = points[0]
-				builder.Move(points[0])
+				builder.Move(gcb.BetterPt[gcb.AbsolutePos](gcb.AbsolutePos(cmd.M[0]*s.scale), gcb.AbsolutePos(cmd.M[1]*s.scale)))
 				builder.BeginContinousLine()
-			case PathLineToAbs, PathLineToRel:
-				points, err = s.readNPts(txts, &i, 1)
-				if err != nil {
-					return builder, err
-				}
-
-				if currentType == PathLineToRel {
-					for i, pt := range points {
-						points[i] = builder.RelToAbs(gcb.Redefine[gcb.RelativePos](pt))
-					}
-				}
-
-				glg.Debugf("Drawing line to %v", points[0])
-				builder.DrawLine(builder.Current(), points[0])
-			case PathCubicBezierCurveRel, PathCubicBezierCurveAbs:
-				points, err = s.readNPts(txts, &i, 3)
-				if err != nil {
-					return builder, err
-				}
-
-				if currentType == PathCubicBezierCurveRel {
-					for i, pt := range points {
-						points[i] = builder.RelToAbs(gcb.Redefine[gcb.RelativePos](pt))
-					}
-				}
-
-				glg.Debugf("Drawing cubic bezier curve from: c1: %v c2: %v end: %v", points[0], points[1], points[2])
-
-				if err := builder.DrawBezier(20, builder.Current(), points[0], points[1], points[2]); err != nil {
-					return builder, err
-				}
-
-			case PathCloseAbs, PathCloseRel:
-				glg.Debugf("Close path to %v", start)
-				builder.DrawLine(builder.Current(), start)
-			default:
-				return builder, fmt.Errorf("%s: %w", currentType, errors.New("Not Implemented"))
+			case svg.CircleInstruction:
+				glg.Warn("Circle not implemented")
+			case svg.CurveInstruction:
+				builder.DrawBezier(
+					10,
+					builder.Current(),
+					gcb.BetterPt(gcb.AbsolutePos(cmd.CurvePoints.C1[0]*s.scale), gcb.AbsolutePos(cmd.CurvePoints.C1[1]*s.scale)),
+					gcb.BetterPt(gcb.AbsolutePos(cmd.CurvePoints.C2[0]*s.scale), gcb.AbsolutePos(cmd.CurvePoints.C2[1]*s.scale)),
+					gcb.BetterPt(gcb.AbsolutePos(cmd.CurvePoints.T[0]*s.scale), gcb.AbsolutePos(cmd.CurvePoints.T[1]*s.scale)),
+				)
+			case svg.LineInstruction:
+				builder.DrawLine(
+					builder.Current(),
+					gcb.BetterPt[gcb.AbsolutePos](gcb.AbsolutePos(cmd.M[0]*s.scale), gcb.AbsolutePos(cmd.M[1]*s.scale)),
+				)
+			case svg.CloseInstruction:
+				glg.Warn("Close not implemented")
+			case svg.PaintInstruction:
+				glg.Warn("Paint not implemented")
+			}
+		case err := <-parsedErr:
+			if err != nil {
+				panic(err)
+				return builder, err
 			}
 		}
 
-		builder.EndContinousLine()
+		/*
+			continue // do to skip the following xd
+			builder.Commentf("Drawing path %d", lineIdx)
+			txts := strings.Split(line.D, " ")
+			var currentType PathType
+			start := gcb.BetterPoint[gcb.AbsolutePos]{0, 0}
 
-		builder.Separator()
-		glg.Debugf("Path %d done. Resetting to 0,0.", lineIdx)
-		builder.Move(gcb.BetterPt[gcb.AbsolutePos](0, 0))
+			builder.BeginContinousLine()
+			for i := 0; i < len(txts); {
+				t := txts[i]
+				glg.Debugf("Proessing %d/%d: %s", i, len(txts), t)
+				pathType, ok := PathTypeEnum[t]
+				if ok {
+					currentType = pathType
+					glg.Debugf("Setting current operation type to %s", currentType)
+					i++
+				}
+
+				var points []gcb.BetterPoint[gcb.AbsolutePos]
+				switch currentType {
+				case PathMoveToRel, PathMoveToAbs:
+					builder.EndContinousLine()
+					points, err = s.readNPts(txts, &i, 1)
+					if err != nil {
+						return builder, err
+					}
+
+					if currentType == PathMoveToRel {
+						for i, pt := range points {
+							points[i] = builder.RelToAbs(gcb.Redefine[gcb.RelativePos](pt))
+						}
+					}
+
+					glg.Debugf("Moving to %v", points[0])
+					start = points[0]
+					builder.Move(points[0])
+					builder.BeginContinousLine()
+				case PathLineToAbs, PathLineToRel:
+					points, err = s.readNPts(txts, &i, 1)
+					if err != nil {
+						return builder, err
+					}
+
+					if currentType == PathLineToRel {
+						for i, pt := range points {
+							points[i] = builder.RelToAbs(gcb.Redefine[gcb.RelativePos](pt))
+						}
+					}
+
+					glg.Debugf("Drawing line to %v", points[0])
+					builder.DrawLine(builder.Current(), points[0])
+				case PathCubicBezierCurveRel, PathCubicBezierCurveAbs:
+					points, err = s.readNPts(txts, &i, 3)
+					if err != nil {
+						return builder, err
+					}
+
+					if currentType == PathCubicBezierCurveRel {
+						for i, pt := range points {
+							points[i] = builder.RelToAbs(gcb.Redefine[gcb.RelativePos](pt))
+						}
+					}
+
+					glg.Debugf("Drawing cubic bezier curve from: c1: %v c2: %v end: %v", points[0], points[1], points[2])
+
+					if err := builder.DrawBezier(20, builder.Current(), points[0], points[1], points[2]); err != nil {
+						return builder, err
+					}
+
+				case PathCloseAbs, PathCloseRel:
+					glg.Debugf("Close path to %v", start)
+					builder.DrawLine(builder.Current(), start)
+				default:
+					return builder, fmt.Errorf("%s: %w", currentType, errors.New("Not Implemented"))
+				}
+			}
+
+			builder.EndContinousLine()
+
+			builder.Separator()
+			glg.Debugf("Path %d done. Resetting to 0,0.", lineIdx)
+			builder.Move(gcb.BetterPt[gcb.AbsolutePos](0, 0))
+		*/
 	}
 
 	// 2.0: draw circles
-	for _, c := range s.Graph.Circles {
-		builder.DrawCircleFilled(gcb.BetterPoint[gcb.AbsolutePos]{gcb.AbsolutePos(c.Cx * s.scale), gcb.AbsolutePos(c.Cy * s.scale)}, (c.R * s.scale))
-		builder.Separator()
-	}
+	/*
+		for _, c := range s.Graph.Circles {
+			builder.DrawCircleFilled(gcb.BetterPoint[gcb.AbsolutePos]{gcb.AbsolutePos(c.Cx * s.scale), gcb.AbsolutePos(c.Cy * s.scale)}, (c.R * s.scale))
+			builder.Separator()
+		}
+	*/
 
 	// 2.1: draw rects
-	for _, r := range s.Graph.Rects {
-		builder.DrawRectFilled(gcb.BetterPt(gcb.AbsolutePos(r.X*s.scale), gcb.AbsolutePos(r.Y*s.scale)), gcb.BetterPt(gcb.AbsolutePos(r.X*s.scale+r.W*s.scale), gcb.AbsolutePos(r.Y*s.scale+r.H*s.scale)))
-	}
+	/*
+		for _, r := range s.Graph.Rects {
+			builder.DrawRectFilled(gcb.BetterPt(gcb.AbsolutePos(r.X*s.scale), gcb.AbsolutePos(r.Y*s.scale)), gcb.BetterPt(gcb.AbsolutePos(r.X*s.scale+r.W*s.scale), gcb.AbsolutePos(r.Y*s.scale+r.H*s.scale)))
+		}
+	*/
 
 	return builder, nil
 }
