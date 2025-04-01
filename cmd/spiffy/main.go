5b32f45fa920871fa6d51ea05d8864c7c64ce685
gucio321 <gucio321@protonmail.com>
2025-04-01 10:10:37 +0200

    main.go: add json presets for more convinience

--- cmd/spiffy/main.go
+++ cmd/spiffy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -14,40 +15,65 @@ import (
 	"github.com/gucio321/spiffy/pkg/viewer"
 )
 
-type flags struct {
-	inputFilePath  string
-	outputFilePath string
-	scale          float64
-	commentsAbove  bool
-	noLineComments bool
-	view           bool
-	repeatN        int
-	repeatDepth    float64
+type Flags struct {
+	InputFilePath  string
+	OutputFilePath string
+	Scale          float64
+	CommentsAbove  bool
+	NoLineComments bool
+	View           bool
+	RepeatN        int
+	RepeatDepth    float64
 	startZ         float64
-	depthDelta     float64
+	DepthDelta     float64
 	force          bool
+	preset         string
+	makePreset     bool
 }
 
 func main() {
-	var f flags
-	flag.StringVar(&f.inputFilePath, "i", "", "input file path")
-	flag.StringVar(&f.outputFilePath, "o", "", "output file path")
-	flag.Float64Var(&f.scale, "s", 1.0, "scale factor")
-	flag.BoolVar(&f.noLineComments, "nlc", false, "no line comments")
-	flag.BoolVar(&f.commentsAbove, "ca", false, "comments above")
-	flag.BoolVar(&f.view, "v", false, "view")
-	flag.IntVar(&f.repeatN, "rn", 0, "repeat N times (use with -rd)")
-	flag.Float64Var(&f.repeatDepth, "rd", 5, "repeat depth (use with -rn)")
+	var f Flags
+	flag.StringVar(&f.InputFilePath, "i", "", "input file path")
+	flag.StringVar(&f.OutputFilePath, "o", "", "output file path")
+	flag.Float64Var(&f.Scale, "s", 1.0, "Scale factor")
+	flag.BoolVar(&f.NoLineComments, "nlc", false, "no line comments")
+	flag.BoolVar(&f.CommentsAbove, "ca", false, "comments above")
+	flag.BoolVar(&f.View, "v", false, "view")
+	flag.IntVar(&f.RepeatN, "rn", 0, "repeat N times (use with -rd)")
+	flag.Float64Var(&f.RepeatDepth, "rd", 5, "repeat depth (use with -rn)")
 	flag.Float64Var(&f.startZ, "sz", 0, "start Z (use along with -dz for delta zet)")
-	flag.Float64Var(&f.depthDelta, "dz", gcb.BaseDepth, "delta Z (use along with -sz for start zet)")
+	flag.Float64Var(&f.DepthDelta, "dz", gcb.BaseDepth, "delta Z (use along with -sz for start zet)")
 	flag.BoolVar(&f.force, "f", false, "force")
+	flag.StringVar(&f.preset, "preset", "", "JSOn preset file path. This will override all other flags")
+	flag.BoolVar(&f.makePreset, "make-preset", false, "auto-generate preset")
 	flag.Parse()
 
-	if f.startZ != 0 && f.depthDelta == gcb.BaseDepth && !f.force {
+	if f.makePreset {
+		out, err := json.MarshalIndent(f, "", "\t")
+		if err != nil {
+			glg.Fatalf("Unable to generate preset: %v", err)
+		}
+		fmt.Println(string(out))
+		glg.Infof("Presets generated")
+		return
+	}
+
+	if f.preset != "" {
+		data, err := os.ReadFile(f.preset)
+		if err != nil {
+			glg.Fatalf("Unable to read preset from %s: %v (use valid file or empty to not use presets)", f.preset, err)
+		}
+
+		if err := json.Unmarshal(data, &f); err != nil {
+			glg.Fatalf("Unable to parse preset from %s: %v", f.preset, err)
+		}
+	}
+
+	if f.startZ != 0 && f.DepthDelta == gcb.BaseDepth && !f.force {
 		glg.Fatal("Please specify -dz (-f to force)")
 	}
 
-	if _, err := os.Stat(f.inputFilePath); os.IsNotExist(err) {
+	if _, err := os.Stat(f.InputFilePath); os.IsNotExist(err) {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -59,9 +85,9 @@ func main() {
 
 	defer inkscapeProxy.Close()
 
-	convertedFile := f.inputFilePath + ".spiffy.svg"
+	convertedFile := f.InputFilePath + ".spiffy.svg"
 	inkscapeProxy.RawCommands(
-		fmt.Sprintf("file-open:%s", f.inputFilePath),
+		fmt.Sprintf("file-open:%s", f.InputFilePath),
 		fmt.Sprintf("export-filename:%s", convertedFile),
 		"export-type:svg",
 		"select-all",
@@ -72,40 +98,40 @@ func main() {
 
 	data, err := os.ReadFile(convertedFile)
 	if err != nil {
-		glg.Fatalf("Cannot read file %s: %v", f.inputFilePath, err)
+		glg.Fatalf("Cannot read file %s: %v", f.InputFilePath, err)
 	}
 
 	result, err := pkg.Parse(data)
 	if err != nil {
-		glg.Fatalf("Cannot parse file %s: %v", f.inputFilePath, err)
+		glg.Fatalf("Cannot parse file %s: %v", f.InputFilePath, err)
 	}
 
-	if f.repeatN > 0 {
-		result.Repeat(f.repeatN, f.repeatDepth)
+	if f.RepeatN > 0 {
+		result.Repeat(f.RepeatN, f.RepeatDepth)
 	}
 
 	if f.startZ != 0 {
-		result.Depths(f.depthDelta, f.startZ)
+		result.Depths(f.DepthDelta, f.startZ)
 	}
 
-	result.Scale(float32(f.scale))
+	result.Scale(float32(f.Scale))
 	gcode, err := result.GCode()
 	if err != nil {
 		gcode.Dump()
 		glg.Fatalf("Cannot generate GCode: %v", err)
 	}
 
-	gcode.Comments(!f.noLineComments, f.commentsAbove)
+	gcode.Comments(!f.NoLineComments, f.CommentsAbove)
 
 	fmt.Println(gcode)
 
-	if f.outputFilePath != "" {
-		if err := os.WriteFile(f.outputFilePath, []byte(gcode.String()), 0644); err != nil {
-			glg.Fatalf("Cannot write file %s: %v", f.outputFilePath, err)
+	if f.OutputFilePath != "" {
+		if err := os.WriteFile(f.OutputFilePath, []byte(gcode.String()), 0644); err != nil {
+			glg.Fatalf("Cannot write file %s: %v", f.OutputFilePath, err)
 		}
 	}
 
-	if f.view {
+	if f.View {
 		ebiten.SetWindowSize(800, 600)
 		if err := ebiten.RunGame(viewer.NewViewer(gcode)); err != nil {
 			glg.Fatalf("Cannot run viewer: %v", err)
